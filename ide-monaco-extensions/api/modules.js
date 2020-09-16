91a2ba013b3cfade2d697533123f4749ae62e05f
ThuF <jordan.p.pavlov@gmail.com>
2020-09-16 15:13:57 +0300

    Add support for listing of published modules

--- ide-monaco-extensions/api/modules.js
+++ ide-monaco-extensions/api/modules.js
@@ -1,5 +1,6 @@
 var response = require("http/v4/response");
 var extensions = require("core/v4/extensions");
+var modulesParser = require("ide-monaco-extensions/api/utils/modulesParser");
 
 var modules = [];
 var apiModulesExtensions = extensions.getExtensions("api-modules");
@@ -8,5 +9,8 @@ apiModulesExtensions.forEach(function(apiModule) {
 	var module = require(apiModule);
 	modules = modules.concat(module.getContent());
 });
+
+let repositoryModules = modulesParser.getModules();
+modules = modules.concat(repositoryModules);
 
 response.println(JSON.stringify(modules));
