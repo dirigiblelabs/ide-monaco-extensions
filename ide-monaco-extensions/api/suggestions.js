f216e4d44b97c5c9f36802956049f1339ad7668b
ThuF <jordan.p.pavlov@gmail.com>
2021-04-10 07:42:48 +0000

    Adds "moduleInfoCache"

--- ide-monaco-extensions/api/suggestions.js
+++ ide-monaco-extensions/api/suggestions.js
@@ -1,44 +1,9 @@
-var response = require("http/v4/response");
-var request = require("http/v4/request");
-var repository = require("platform/v4/repository");
-var config = require("core/v4/configurations");
-var modulesParser = require("ide-monaco-extensions/api/utils/modulesParser");
-var suggestionsParser = require("ide-monaco-extensions/api/utils/suggestionsParser");
+let response = require("http/v4/response");
+let request = require("http/v4/request");
+let moduleInfoCache = require("ide-monaco-extensions/api/utils/moduleInfoCache");
 
-const PATH_REGISTRY_PUBLIC = "/registry/public";
-const MODULE_INFO_PREFIX = "MODULE_INFO_";
-
-let moduleName = request.getParameter("moduleName");
-let moduleInfo = loadModuleInfo(moduleName);
-
-try {
-    let resource = repository.getResource(`${PATH_REGISTRY_PUBLIC}/${moduleName}.js`);
-    let information = resource.getInformation();
-    let lastModifiedAt = information.getModifiedAt().getTime();
-    if (isEmptyObject(moduleInfo) || moduleInfo.lastModifiedAt < lastModifiedAt) {
-        moduleInfo = {
-            moduleName: moduleName,
-            lastModifiedAt: lastModifiedAt,
-            suggestions: suggestionsParser.parse(moduleName)
-        }
-        saveModuleInfo(moduleInfo);
-    }
-} catch (e) {
-    console.error(`Error occured ${e}`);
-}
+let moduleInfo = moduleInfoCache.get(request.getParameter("moduleName"));
 
 response.print(JSON.stringify(moduleInfo.suggestions));
 response.flush();
 response.close();
-
-function loadModuleInfo(moduleName) {
-    return JSON.parse(config.get(MODULE_INFO_PREFIX + moduleName, "{}"));
-}
-
-function saveModuleInfo(moduleInfo) {
-    config.set(`${MODULE_INFO_PREFIX}${moduleInfo.moduleName}`, JSON.stringify(moduleInfo));
-}
-
-function isEmptyObject(obj) {
-    return obj && Object.keys(obj).length === 0 && obj.constructor === Object
-}
---
+++ ide-monaco-extensions/api/utils/moduleInfoCache.js
@@ -0,0 +1,73 @@
+var repository = require("platform/v4/repository");
+var config = require("core/v4/configurations");
+var modulesParser = require("ide-monaco-extensions/api/utils/modulesParser");
+var suggestionsParser = require("ide-monaco-extensions/api/utils/suggestionsParser");
+
+const PATH_REGISTRY_PUBLIC = "/registry/public";
+const MODULE_INFO_PREFIX = "MODULE_INFO_";
+const PRIORITY_MODULES = [
+    "http/v4/response",
+    "http/v4/request",
+    "db/v4/dao",
+    "db/v4/sql",
+    "db/v4/query",
+    "db/v4/update",
+    "db/v4/database",
+    "core/v4/configurations",
+    "security/v4/user"
+];
+
+exports.get = function(moduleName) {
+    let moduleInfo = loadModuleInfo(moduleName);
+    try {
+        let resource = repository.getResource(`${PATH_REGISTRY_PUBLIC}/${moduleName}.js`);
+        let information = resource.getInformation();
+        let lastModifiedAt = information.getModifiedAt().getTime();
+        if (isEmptyObject(moduleInfo) || moduleInfo.lastModifiedAt < lastModifiedAt) {
+            moduleInfo = {
+                moduleName: moduleName,
+                lastModifiedAt: lastModifiedAt,
+                suggestions: suggestionsParser.parse(moduleName)
+            }
+            saveModuleInfo(moduleInfo);
+        }
+    } catch (e) {
+        console.error(`Error occured ${e}`);
+    }
+    return moduleInfo;
+};
+
+exports.refresh = function() {
+    modulesParser.getModules()
+        .sort((a, b) => {
+            let isPriorityModuleA = PRIORITY_MODULES.includes(a.name);
+            let isPriorityModuleB = PRIORITY_MODULES.includes(b.name);
+            if (isPriorityModuleA && !isPriorityModuleB) {
+                return -1;
+            } else if (!isPriorityModuleA && isPriorityModuleB) {
+                return 1;
+            }
+            return 0;
+        })
+        .forEach(e => {
+            console.log(`Refreshing ModuleInfo: ${e.name}`);
+            exports.get(e.name)
+        });
+};
+
+exports.clear = function() {
+    let keys = config.getKeys().filter(e => e.startsWith(MODULE_INFO_PREFIX));
+    keys.forEach(key => config.remove(key));
+};
+
+function loadModuleInfo(moduleName) {
+    return JSON.parse(config.get(MODULE_INFO_PREFIX + moduleName, "{}"));
+}
+
+function saveModuleInfo(moduleInfo) {
+    config.set(`${MODULE_INFO_PREFIX}${moduleInfo.moduleName}`, JSON.stringify(moduleInfo));
+}
+
+function isEmptyObject(obj) {
+    return obj && Object.keys(obj).length === 0 && obj.constructor === Object
+}
